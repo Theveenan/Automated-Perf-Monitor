349568c0c5c61035d5f3365a3b4545cf0e534718
Theveenan <theveenan@gmail.com>
2023-08-20 01:33:21 -0400

    Data extract/cleaning/trans done

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM apache/airflow:2.6.3-python3.8
+RUN pip install pandas
+RUN pip install numpy
+
--- dags/pythonOperatorDag.py
+++ dags/pythonOperatorDag.py
@@ -2,10 +2,10 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 import subprocess
-
-
+from airflow.contrib.sensors.file_sensor import FileSensor
 import os
-
+import pandas as pd
+import numpy as np
 
 default_args = {
     'owner': 'thev',
@@ -14,25 +14,52 @@ default_args = {
 
 }
 
-def provide_question():
-    followUp="How are you?"
+def send_transformed_data():
+    followUp="send transformed data to a database"
     return followUp
 
-def greet(name, ti):
-    followUp = ti.xcom_pull(task_ids='provide_question_task')
+def delete_data(name, ti):
+    followUp = ti.xcom_pull(task_ids='send_transformed_data_task')
     print(f"hello {name}! {followUp}")
 
-def start_logging():
-    command = 'logman start "New Data Collector Set"'
-    print("starting logging")
+def start_transforming():
+    #command = 'logman start "New Data Collector Set"'
+    #print("starting logging")
     
-    current = os.getcwd()
-    print("current:",current)
-    target_dir=(current+"/data")
-    print("target:",target_dir)
+    # current = os.getcwd()
+    # print("current:",current)
+    # target_dir=(current+"/data")
+    # print("target:",target_dir)
+    # target_dir = os.getcwd()+"/data/DESKTOP-67P0ISA_DataCollector01.csv"
+    # data_frame = pd.read_csv(target_dir)
+    # print(data_frame)
+    # print(data_frame[0])
+
+    target_dir = os.getcwd()+"/data/DESKTOP-67P0ISA_DataCollector01.csv"
+    data_frame = pd.read_csv(target_dir)
+
+    new_column_name = {'(PDH-CSV 4.0) (Eastern Daylight Time)(240)': ' Date Time'}
+    data_frame.rename(columns=new_column_name, inplace=True)
+
+    memory_ordered_filtered =  [data_frame.columns[0]] + [col for col in data_frame.columns if "Memory" in col]
+    memory_data = data_frame[memory_ordered_filtered]
+
+    processor_ordered_filtered =  [data_frame.columns[0]] + [col for col in data_frame.columns if "rocessor" in col]
+    processor_data = data_frame[processor_ordered_filtered]
+
+    datas=[memory_data, processor_data]
+
+    for x in range(0,len(datas)):
+        white_space_removed = datas[x].applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        empty_values_filled = white_space_removed.replace("", "0")
+        empty_values_filled.columns = empty_values_filled.columns.str.lstrip("\\DESKTOP-67P0ISA\\")
+        empty_values_filled.columns = empty_values_filled.columns.str.lstrip("Memory")
+        empty_values_filled.columns = empty_values_filled.columns.str.lstrip("rocessor")
+        output_filename = os.getcwd()+f'/data/output_{x}.csv'
+        empty_values_filled.to_csv(output_filename, index=False, encoding='utf-8')
 
     # Specify the file name you want to delete
-    file_to_delete = "delete.txt"
+    #file_to_delete = "delete.txt"
 
     # Get the current script's directory
     #current_dir = os.path.dirname(__file__)
@@ -40,8 +67,8 @@ def start_logging():
     # Navigate two levels up to reach the parent directory
 
     # Construct the full path to the file
-    file_path = os.path.join(target_dir, file_to_delete)
-    os.remove(file_path)
+    #file_path = os.path.join(target_dir, file_to_delete)
+    #os.remove(file_path)
     #subprocess.run(command, shell=False)
 
 with DAG(
@@ -49,22 +76,34 @@ with DAG(
     dag_id='my_first_python_dag',
     description='my first dag using python operator',
     start_date=datetime(2023, 8, 16),
-    schedule_interval='@daily'
+    schedule_interval=timedelta(minutes=1)
 ) as dag:
     task1= PythonOperator(
-        task_id='greet',
-        python_callable=greet,
+        task_id='delete_data_task',
+        python_callable=delete_data,
         op_kwargs={'name': 'thev'}
     )
 
     task2 = PythonOperator(
-        task_id = 'provide_question_task',
-        python_callable = provide_question
+        task_id = 'send_transformed_data_task',
+        python_callable = send_transformed_data
     )
 
     task3 = PythonOperator(
-        task_id = 'start_logging',
-        python_callable = start_logging
+        task_id = 'start_transforming',
+        python_callable = start_transforming
+    ) 
+
+    target_dir = os.getcwd()+"/data/DESKTOP-67P0ISA_DataCollector01.csv"
+
+    wait_for_file_task = FileSensor(
+        task_id='wait_for_file_task',
+        filepath=target_dir,
+        poke_interval=5,  # Set the poke interval in seconds
+        timeout=16,
+        fs_conn_id='file_system',
+        mode='poke'
     )
 
-    task2 >> task1 >> task3
+
+    wait_for_file_task >> task3
